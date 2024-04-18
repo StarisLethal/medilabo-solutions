f9dbbdc2b2b62a1449c7ec6f5d9e0facfcc511eb
Amenor <lethality.game@gmail.com>
2024-04-18 11:40:08 +0200

    Initial commit

---
+++ ms-patients/src/main/java/com/amenor/openclassrooms/mspatients/MsPatientsApplication.java
@@ -0,0 +1,13 @@
+package com.amenor.openclassrooms.mspatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPatientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPatientsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/amenor/openclassrooms/medilabosolutions/MedilaboSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.amenor.openclassrooms.medilabosolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboSolutionsApplication.class, args);
+	}
+
+}
