a798496c3925fa1195f77affacb4d0a563f8d6f1
Amenor <lethality.game@gmail.com>
2024-04-28 12:38:35 +0200

    Initial commit

---
+++ ms-config/src/test/java/com/amenor/openclassrooms/msconfig/MsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amenor.openclassrooms.msconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
