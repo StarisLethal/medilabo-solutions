5120cce78cb7e53ab02fa3ccb44660ad92ad62c2
Amenor <lethality.game@gmail.com>
2024-04-28 18:22:40 +0200

    Update building discovery

--- ms-patients/src/main/java/com/amenor/openclassrooms/mspatients/MsPatientsApplication.java
+++ ms-patients/src/main/java/com/amenor/openclassrooms/mspatients/MsPatientsApplication.java
@@ -2,10 +2,12 @@ package com.amenor.openclassrooms.mspatients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.amenor.openclassrooms")
+@EnableDiscoveryClient
 public class MsPatientsApplication {
 
     public static void main(String[] args) {
