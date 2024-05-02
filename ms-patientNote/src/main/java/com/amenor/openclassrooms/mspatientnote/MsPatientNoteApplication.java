e4b614a9bbe28f2efb21370f681ab5fa9f4ab827
Amenor <lethality.game@gmail.com>
2024-05-02 17:26:26 +0200

    init microservice for note

---
+++ ms-patientNote/src/main/java/com/amenor/openclassrooms/mspatientnote/MsPatientNoteApplication.java
@@ -0,0 +1,13 @@
+package com.amenor.openclassrooms.mspatientnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPatientNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPatientNoteApplication.class, args);
+    }
+
+}
---
+++ ms-patientNote/src/test/java/com/amenor/openclassrooms/mspatientnote/MsPatientNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amenor.openclassrooms.mspatientnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPatientNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
